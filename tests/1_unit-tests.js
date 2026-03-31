5ef33cb378e807c3c94f15157241f894b9aae9e3
Najart200 <Najartraufawuni@gmail.com>
2026-03-31 15:44:16 +0000

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -6,17 +6,13 @@ suite('Unit Tests', function () {
     // #1
     test('#isNull, #isNotNull', function () {
       assert.isNull(null, 'This is an optional error description - e.g. null is null');
-<<<<<<< HEAD
       assert.isNotNull(1, '1 is not null');
-=======
-      assert.isNotNull(1, '1 is not null');
->>>>>>> 40ea158a8d3ec31fda7750c10578b4959da53f99
     });
     // #2
     test('#isDefined, #isUndefined', function () {
-      assert.fail(null, 'null is not undefined');
-      assert.fail(undefined, 'undefined IS undefined');
-      assert.fail('hello', 'A string is not undefined');
+      assert.isDefined(null, 'null is not undefined');
+      assert.isUndefined(undefined, 'undefined IS undefined');
+      assert.isDefined('hello', 'A string is not undefined');
     });
     // #3
     test('#isOk, #isNotOk', function () {
